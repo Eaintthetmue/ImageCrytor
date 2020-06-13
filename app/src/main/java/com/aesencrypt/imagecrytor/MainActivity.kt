3627b67430926862a78c528702420f0930a95419
Hay Mar San <haymarsan11@gmail.com>
2020-06-13 11:45:33 +0630

    Feature Update: Initial commit

--- app/src/main/java/com/aesencrypt/imagecrytor/MainActivity.kt
+++ app/src/main/java/com/aesencrypt/imagecrytor/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.aesencrypt.imagecrytor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aesencrypt.imagecrytor.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEncryptKey.setOnClickListener {
+            startActivity(Intent(this, EncryptionActivity::class.java))
+        }
+
+        btnDecryptionKey.setOnClickListener {
+            startActivity(Intent(this, DecryptionActivity::class.java))
+        }
+
     }
 }
